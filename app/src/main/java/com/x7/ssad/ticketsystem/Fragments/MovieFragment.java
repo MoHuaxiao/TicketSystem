5a3669de7a9f343ca4dfe358908127b89805837f
yongningfu <535802703@qq.com>
2017-05-28 17:41:03 +0800

    加入底部tablayout

---
+++ app/src/main/java/com/x7/ssad/ticketsystem/Fragments/MovieFragment.java
@@ -0,0 +1,22 @@
+package com.x7.ssad.ticketsystem.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.x7.ssad.ticketsystem.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MovieFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_movie, container, false);
+    }
+}
