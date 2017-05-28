067ab19bdfdd14e044d3ad8b0597d5f2c76d0aea
yongningfu <535802703@qq.com>
2017-05-28 18:12:01 +0800

    整合之前的代码

--- app/src/main/java/com/x7/ssad/ticketsystem/Fragments/MovieFragment.java
+++ app/src/main/java/com/x7/ssad/ticketsystem/Fragments/MovieFragment.java
@@ -6,7 +6,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
+import com.x7.ssad.ticketsystem.Adapters.HotOnAirMovieAdapter;
+import com.x7.ssad.ticketsystem.Backend.BackendStub;
+import com.x7.ssad.ticketsystem.Model.Movie;
 import com.x7.ssad.ticketsystem.R;
 
 /**
@@ -17,6 +25,18 @@ public class MovieFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movie, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_movie, container, false);
+        BackendStub backend = BackendStub.getInstance();
+
+        RecyclerView HotOnAirMovieList = (RecyclerView) rootView.findViewById(R.id.hot_on_air_movie_recycview);
+
+        LinearLayoutManager mLLM = new LinearLayoutManager(getActivity().getApplicationContext());
+        mLLM.setOrientation(LinearLayoutManager.VERTICAL);
+        HotOnAirMovieList.setLayoutManager(mLLM);
+
+        Log.d("Activity", Integer.toString(backend.getHotOnAirMovies().size()));
+
+        HotOnAirMovieList.setAdapter(new HotOnAirMovieAdapter(getActivity().getApplicationContext(), backend));
+        return rootView;
     }
 }
