9015d363a9e09a11336356faf2a6301ae7de3614
yongningfu <535802703@qq.com>
2017-06-03 03:08:25 +0800

    加入影院列表

--- app/src/main/java/com/x7/ssad/ticketsystem/Fragments/CinemaFragment.java
+++ app/src/main/java/com/x7/ssad/ticketsystem/Fragments/CinemaFragment.java
@@ -6,8 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import com.x7.ssad.ticketsystem.Adapters.CinemaAdapter;
+import com.x7.ssad.ticketsystem.Model.Cinema;
 import com.x7.ssad.ticketsystem.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,6 +23,17 @@ public class CinemaFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cinema, container, false);
+        View rootView =  inflater.inflate(R.layout.fragment_cinema, container, false);
+        ListView cinemaListView = (ListView) rootView.findViewById(R.id.cinemaListView);
+
+        List<Cinema> cinemaList = new ArrayList<Cinema>();
+        Cinema c;
+        for (int i = 0; i < 10; i++) {
+            c = new Cinema("今日珠江国际影城", "番禺区小谷围贝岗二横路1好店 新天地店", 30);
+            cinemaList.add(c);
+        }
+        CinemaAdapter cinemaAdapter = new CinemaAdapter(cinemaList, getActivity());
+        cinemaListView.setAdapter(cinemaAdapter);
+        return rootView;
     }
 }
