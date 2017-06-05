91d12eb0b108bf41eb3294e15767b8352aadbf4a
yongningfu <535802703@qq.com>
2017-06-06 03:24:20 +0800

    增加滑动图片

--- app/src/main/java/com/x7/ssad/ticketsystem/Activities/CinemaDetail.java
+++ app/src/main/java/com/x7/ssad/ticketsystem/Activities/CinemaDetail.java
@@ -1,20 +1,34 @@
 package com.x7.ssad.ticketsystem.Activities;
 
 import android.content.Intent;
+import android.database.CursorJoiner;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.x7.ssad.ticketsystem.Adapters.MovieShotBlockAdapter;
 import com.x7.ssad.ticketsystem.Adapters.TicketAdapter;
+import com.x7.ssad.ticketsystem.Backend.BackendStub;
+import com.x7.ssad.ticketsystem.Model.Cinema;
+import com.x7.ssad.ticketsystem.Model.Movie;
 import com.x7.ssad.ticketsystem.Model.Ticket;
 import com.x7.ssad.ticketsystem.R;
+import com.x7.ssad.ticketsystem.Session.SessionManager;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class CinemaDetail extends AppCompatActivity {
+
+    private SessionManager SM;
+    private BackendStub backend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +43,19 @@ public class CinemaDetail extends AppCompatActivity {
         }
         TicketAdapter ticketAdapter = new TicketAdapter(ticketList, this);
         ticketListView.setAdapter(ticketAdapter);
+
+        RecyclerView movieShootRecyclerView = (RecyclerView)findViewById(R.id.movieShot);
+        backend = BackendStub.getInstance();
+        SM = SessionManager.getInstance();
+        // 获取数据
+//        Movie CurrentMovie = backend.getMovieByID(SM.getMyMovieID());
+        int movieshotIdList[] = {R.mipmap.movie_shot, R.mipmap.movie_shot,
+                R.mipmap.movie_shot, R.mipmap.movie_shot, R.mipmap.movie_shot};
+
+        MovieShotBlockAdapter movieShotBlockAdapter = new MovieShotBlockAdapter(getApplicationContext(), movieshotIdList);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(CinemaDetail.this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        movieShootRecyclerView.setLayoutManager(linearLayoutManager);
+        movieShootRecyclerView.setAdapter(movieShotBlockAdapter);
     }
 }
