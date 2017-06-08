e0ec562c6242eaf5eb3397ae88941a06264eb003
isVoid <michaelwang0905@icloud.com>
2017-06-08 10:35:45 +0800

    Trying Retrofit

--- app/src/main/java/com/x7/ssad/ticketsystem/Backend/BackendStub.java
+++ app/src/main/java/com/x7/ssad/ticketsystem/Backend/BackendStub.java
@@ -4,11 +4,15 @@ import android.util.Log;
 
 import com.x7.ssad.ticketsystem.Cache.CinemaCacheStub;
 import com.x7.ssad.ticketsystem.Cache.MovieCacheStub;
+import com.x7.ssad.ticketsystem.Cache.UserDAOStub;
 import com.x7.ssad.ticketsystem.Model.Cinema;
 import com.x7.ssad.ticketsystem.Model.Movie;
+import com.x7.ssad.ticketsystem.Model.User;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Retrofit;
 
 /**
  * Created by WangYinghao on 5/26/17.
@@ -16,15 +20,28 @@ import java.util.List;
 
 //TODO: Change this to a real backend communication system.
 public class BackendStub {
+
+
     private static BackendStub ourInstance = new BackendStub();
     List<String> USERS;
     List<String> PASSWORDS;
+
+    UserDAOStub UD;
     CinemaCacheStub CCS;
     MovieCacheStub MCS;
+
+    Retrofit retrofit;
 
     private BackendStub() {
         USERS = new ArrayList<>();
         PASSWORDS = new ArrayList<>();
+
+        UD = new UserDAOStub();
+        CCS = new CinemaCacheStub();
+        MCS = new MovieCacheStub();
+
+        retrofit = new Retrofit.Builder().baseUrl("http://localhost:8081").build();
+
     }
 
     public static BackendStub getInstance() {
@@ -36,9 +53,6 @@ public class BackendStub {
 
         USERS.add(username);
         PASSWORDS.add(password);
-
-        CCS = new CinemaCacheStub();
-        MCS = new MovieCacheStub();
 
         Log.d("Backend Stub", "User Added");
 
@@ -63,6 +77,50 @@ public class BackendStub {
         for (int i = 0; i < USERS.size(); i++) {
             if (username.equals(USERS.get(i))) {
                 if (password.equals(PASSWORDS.get(i))) {
+                    Log.d("Backend Stub", "Log in Successful.");
+                    return true;
+                }
+                else {
+                    Log.d("Backend Stub", "Password Incorrect.");
+                    return false;
+                }
+            }
+        }
+
+        Log.d("Backend Stub", "Unknown Error.");
+        return false;
+
+    }
+
+    //Precondition: Check Duplicate Before Using.
+    public boolean addUser(User u) {
+
+        UD.getUserList().add(u);
+
+        Log.d("Backend Stub", "User Added");
+
+        return true;
+
+    }
+
+    public boolean userExists(User u) {
+
+        for (int i = 0; i < UD.getUserList().size(); i++) {
+            if (u.getEmail().equals(UD.getUserList().get(i).getEmail())) {
+                Log.d("Backend Stub", "User exists.");
+                return true;
+            }
+        }
+        Log.d("Backend Stub", "User not exist.");
+        return false;
+    }
+
+    //Precondition: Check Exist before using.
+    public boolean verifyUser(User u) {
+
+        for (int i = 0; i < UD.getUserList().size(); i++) {
+            if (u.getEmail().equals(UD.getUserList().get(i).getEmail())) {
+                if (u.getPassword().equals(UD.getUserList().get(i).getPassword())) {
                     Log.d("Backend Stub", "Log in Successful.");
                     return true;
                 }
